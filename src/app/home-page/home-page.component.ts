2468d9f039cb2d7d09e915e1adbc0d7c6e233178
ATTALA VENKATA REDDY <36073543+venkat123reddy@users.noreply.github.com>
2024-10-15 20:41:38 -0500

    nav-bar

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [MatToolbarModule,MatIconModule],
+  imports: [MatToolbarModule,MatIconModule,MatSidenavModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
