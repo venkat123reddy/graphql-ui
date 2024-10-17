f0dc8cf227bc9eeecc728d08175df47bcd90e854
ATTALA VENKATA REDDY <36073543+venkat123reddy@users.noreply.github.com>
2024-10-16 23:58:04 -0500

    profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomePageComponent},
+    {path:'user-profile',component:UserProfileComponent}
+];
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,27 @@
+import { UserProfileComponent } from './../user-profile/user-profile.component';
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule,MatDialog} from '@angular/material/dialog';
+
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [MatToolbarModule,MatIconModule,MatSidenavModule],
+  imports: [MatToolbarModule,MatIconModule,MatSidenavModule,MatButtonModule,MatDialogModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+
+  constructor(private dialog :MatDialog){
+
+  }
+
+  userprofile() {
+    this.dialog.open(UserProfileComponent)
+  }
+
 
 }
