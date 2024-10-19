ed2adcbf76d9db22e1ca2177b2304c5113a7894c
ATTALA VENKATA REDDY <36073543+venkat123reddy@users.noreply.github.com>
2024-10-19 01:08:48 -0500

    features

--- src/app/products-view/products-view.component.ts
+++ src/app/products-view/products-view.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-products-view',
   standalone: true,
-  imports: [MatCardModule,MatButtonModule],
+  imports: [MatCardModule,MatButtonModule,MatIconModule,MatInputModule],
   templateUrl: './products-view.component.html',
   styleUrl: './products-view.component.css'
 })
